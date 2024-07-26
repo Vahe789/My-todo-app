1ef0789fbcae1b94feb727d6669c9e43b7a117f5
Vahe789 <155003398+Vahe789@users.noreply.github.com>
2024-07-26 15:45:00 +0400

    Update TodoList.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,20 +1,35 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-class TodoList extends React.Component {
-  render() {
-    const { todos, onDelete } = this.props;
+const TodoList = ({
+  todos,
+  toggleTodo,
+  deleteTodo,
+  searchTerm,
+  filterType,
+}) => {
+  const filteredTodos = todos.filter(
+    (todo) =>
+      todo.text.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      (filterType === "all" ||
+        (filterType === "completed" && todo.completed) ||
+        (filterType === "active" && !todo.completed))
+  );
 
-    return (
-      <div className="task-div">
-        {todos.map((todo) => (
-          <div className="item-div">
-            <TodoItem key={todo.id} todo={todo} onDelete={onDelete} />
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="task-div">
+      {filteredTodos.map((todo) => (
+        <div className="item-div">
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default TodoList;
