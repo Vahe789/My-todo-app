e202566a588fa0a207e5527eb0ac97de4801896b
Vahe789 <155003398+Vahe789@users.noreply.github.com>
2024-07-26 15:46:37 +0400

    Update FilterButtons.js

--- src/FilterButtons.js
+++ src/FilterButtons.js
@@ -1,1 +1,31 @@
+import React from "react";
 
+const FilterButtons = ({ handleFilterChange, filterType }) => {
+  return (
+    <div className="Filtering-buttons">
+      <button
+        className="All-button"
+        onClick={() => handleFilterChange("all")}
+        style={{ fontWeight: filterType === "all" ? "bold" : "normal" }}
+      >
+        All
+      </button>
+      <button
+        className="Completed-button"
+        onClick={() => handleFilterChange("completed")}
+        style={{ fontWeight: filterType === "completed" ? "bold" : "normal" }}
+      >
+        Completed
+      </button>
+      <button
+        className="Not-completed-button"
+        onClick={() => handleFilterChange("active")}
+        style={{ fontWeight: filterType === "active" ? "bold" : "normal" }}
+      >
+        Not Completed
+      </button>
+    </div>
+  );
+};
+
+export default FilterButtons;
